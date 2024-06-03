02d08cde54e2b68c94d09a777e8b211d85618f44
Juriij <juro.kopiciar@gmail.com>
2024-06-03 19:41:09 +0200

    Buttons
    Adding the main buttons to the Main page

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import functions as fun
-import sympy as sp
+import maths as math 
+import interface as int
+
 import tkinter as tk
-import tkinter.font as tkFont
+
 
 
 root = tk.Tk()
  
-# Setup
-root.title("Welcome to Math")
-width = 1000
-height = 700
-root.geometry(f'{str(width)}x{str(height)}')
-
-
-
-# Title text
-fontObj = tkFont.Font(size=40)
-title = tk.Label(root, text="Quadratic Generator", 
-                 height = 5, width = 40, font=fontObj)
-title.pack()
-
+int.setup(root)
 
 
 
