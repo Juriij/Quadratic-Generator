59bb3f1814b37a9792803b7e422bb620fdffe033
Juriij <juro.kopiciar@gmail.com>
2024-06-04 10:29:15 +0200

    Revert "Buttons"
    This reverts commit 02d08cde54e2b68c94d09a777e8b211d85618f44.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import maths as math 
-import interface as int
-
+import functions as fun
+import sympy as sp
 import tkinter as tk
-
+import tkinter.font as tkFont
 
 
 root = tk.Tk()
  
-int.setup(root)
+# Setup
+root.title("Welcome to Math")
+width = 1000
+height = 700
+root.geometry(f'{str(width)}x{str(height)}')
+
+
+
+# Title text
+fontObj = tkFont.Font(size=40)
+title = tk.Label(root, text="Quadratic Generator", 
+                 height = 5, width = 40, font=fontObj)
+title.pack()
+
 
 
 
